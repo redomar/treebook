f45ed104ba4843f47d9da0e1174947306cabe8ae
Mohamed Omar <mohamed12@live.co.uk>
2014-10-22 01:00:18 +0100

    Intial Commit Following TeamTreeHouse ruby tutorial step by step

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '129735b12fb38e2a6bad40278c8783cfd178808105b077670ac99ba7947d3dcf1e683494bd0f872a630a1c96a70905d5173f9e9ec08812dda926f27771964eca'
